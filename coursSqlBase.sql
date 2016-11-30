d1c8d5533e9e10a4af210af926e928f898b3ed91
Coline Martin <colinemartin95@gmail.com>
2016-11-30 16:53:18 +0100

    Troisième commit base de données sql avec les clef primaires, clefs étrangèrès, tables de liason

--- coursSqlBase.sql
+++ coursSqlBase.sql
@@ -1,9 +1,21 @@
+CREATE TABLE Formation (
+	nom VARCHAR(40) NOT NULL,
+	nbHeureTotale FLOAT(5) NULL,
+	id INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY (id),
+
+);
 CREATE TABLE Eleve (
 	nom VARCHAR(40) NOT NULL,
 	prenom VARCHAR(40)NOT NULL,
 	mail VARCHAR(50) NOT NULL UNIQUE,
 	id INTEGER NOT NULL UNIQUE,
-	PRIMARY KEY (id)
+	promotion_id INTEGER NOT NULL,
+	
+	PRIMARY KEY (id),
+	CONSTRAINT eleve_promotion_fk
+	FOREIGN KEY (promotion_id) -- on indique que la colonne promotion_id de notre table porte la contrainte de clef étrangère
+	REFERENCES Promotion (id) -- on indique la table et la colonne cible de notre contraite
 );
 
 CREATE TABLE Formateur (
@@ -20,15 +32,14 @@ CREATE TABLE Promotion (
 	dateDebut DATE NOT NULL,
 	dateFin DATE NOT NULL,
 	id INTEGER NOT NULL UNIQUE,
-	PRIMARY KEY (id)
+	formation_id INTEGER NOT NULL,
+	PRIMARY KEY (id),
+	CONSTRAINT promotion_formation_fk
+	FOREIGN KEY (formation_id)
+	REFERENCES Formation(id)
+	
 );
 
-CREATE TABLE Formation (
-	nom VARCHAR(40) NOT NULL,
-	nbHeureTotale FLOAT(5) NULL,
-	id INTEGER NOT NULL UNIQUE,
-	PRIMARY KEY (id)
-);
 
 CREATE TABLE Matiere(
 	nom VARCHAR(40) NOT NULL,
@@ -36,7 +47,45 @@ CREATE TABLE Matiere(
 	PRIMARY KEY (id)
 );
 
-CREATE TABLE SALLE (
+CREATE TABLE matiere_formateur( -- table de liaison formateur peut enseigner plusieurs matières qui sont différentes -- 
+matiere_id INTEGER NOT NULL,
+formateur_id INTEGER NOT NULL,
+PRIMARY KEY (matiere_id,formateur_id), -- clef primaire composite car composée de plusieurs colonnes/valeurs --
+CONSTRAINT matiere_formateur_matiere_id_fk
+FOREIGN KEY(matiere_id)
+REFERENCES Matiere(id),
+CONSTRAINT matiere_formateur_formateur_id
+FOREIGN KEY(formateur_id)
+REFERENCES Formateur(id)
+
+);
+
+CREATE TABLE matiere_formation(
+matiere_id INTEGER NOT NULL,
+formation_id INTEGER NOT NULL,
+PRIMARY KEY (matiere_id,formation_id),
+CONSTRAINT matiere_formation_matiere_id_fk
+FOREIGN KEY(matiere_id)
+REFERENCES Matiere(id),
+CONSTRAINT matiere_formation_formateur_id
+FOREIGN KEY(formation_id)
+REFERENCES Formation(id)
+
+);
+
+CREATE TABLE Absence(
+eleve_id INTEGER NOT NULL,
+cours_id INTEGER NOT NULL,
+PRIMARY KEY (eleve_id,cours_id),
+CONSTRAINT absence_eleve_id_fk
+FOREIGN KEY (eleve_id)
+REFERENCES Eleve(id),
+CONSTRAINT absence_cours_fk
+FOREIGN KEY (cours_id)
+REFERENCES Cours(id)
+);
+
+CREATE TABLE Salle (
 	nom VARCHAR(40) NOT NULL UNIQUE,
 	capacité INTEGER NOT NULL,
 	id INTEGER NOT NULL UNIQUE,
@@ -44,16 +93,40 @@ CREATE TABLE SALLE (
 	CONSTRAINT salle_unique UNIQUE (nom,capacité)
 );
 
-CREATE TABLE Cours (
+CREATE TABLE Cours ( -- Cours = Session dans le diagramme UML
 	dateDebut DATE NOT NULL,
 	dateFin DATE NOT NULL,
 	id INTEGER NOT NULL UNIQUE,
-	PRIMARY KEY (id) 
+	salle_id INTEGER NOT NULL,
+	formateur_id INTEGER NOT NULL,
+	promotion_id INTEGER NOT NULL,
+	matiere_id INTEGER NOT NULL,
+	PRIMARY KEY (id),
+	-- Clef étrangère pour la salle --
+	CONSTRAINT cours_salle_fk
+	FOREIGN KEY (salle_id)
+	REFERENCES Salle(id),
+	-- Clef étrangère pour le formateur --
+	CONSTRAINT cours_formateur_fk
+	FOREIGN KEY (formateur_id)
+	REFERENCES Formateur(id),
+	-- Clef étrangère pour la promotion --
+	CONSTRAINT cours_promotion_fk
+	FOREIGN KEY (promotion_id)
+	REFERENCES Promotion(id),
+	-- Clef étrangère pour la matière --
+	CONSTRAINT cours_matiere_fk
+	FOREIGN KEY (matiere_id)
+	REFERENCES Matiere(id)
+	
 );
 
-CREATE TABLE EmploiDuTemps (
-	dateDebut DATE NOT NULL,
-	dateFin DATE NOT NULL
-	id INTEGER NOT NULL UNIQUE,
-	PRIMARY KEY (id)
-);
+
+
+-- Pas utile de créer une table EmploiDuTemps car on l'exploitera dans l'application 
+-- CREATE TABLE EmploiDuTemps (
+	-- dateDebut DATE NOT NULL,
+	-- dateFin DATE NOT NULL
+	-- id INTEGER NOT NULL UNIQUE,
+	-- PRIMARY KEY (id)
+-- );
