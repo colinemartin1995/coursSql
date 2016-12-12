a247dd7bc87af5e30f8ba5484e68ab9986bedfc1
Coline Martin <colinemartin95@gmail.com>
2016-12-12 11:30:32 +0100

    modification de exerciceProduit.sql

--- exerciceProduit.sql
+++ exerciceProduit.sql
@@ -1,14 +1,15 @@
 CREATE TABLE Categorie (
 	id INTEGER NOT NULL UNIQUE,
 	nom VARCHAR(40) NOT NULL,
-	PRIMARY KEY (id)
+	CONSTRAINT categorie_pk
+		PRIMARY KEY (id)
 );
 
 CREATE TABLE Produit(
 	id INTEGER NOT NULL UNIQUE,
 	nom VARCHAR(40) NOT NULL,
 	code  INTEGER NOT NULL UNIQUE,
-	categorie_id INTEGER NOT NULL UNIQUE,
+	categorie_id INTEGER, //je ne peux pas avoir de produit sans categorie si pas not null je ne serai pas obligé de lui donner une categorie
 	PRIMARY KEY (id),
 	
 	CONSTRAINT produit_categorie_fk
@@ -16,4 +17,5 @@ CREATE TABLE Produit(
 		REFERENCES Categorie(id)
 
 );
+
 
