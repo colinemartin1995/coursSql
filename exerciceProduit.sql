286515a8e9a86b2b92be1169fb728718b005bd6e
Coline Martin <colinemartin95@gmail.com>
2016-12-12 11:12:46 +0100

    ajout fichier exerciceProduit.sql

---
+++ exerciceProduit.sql
@@ -0,0 +1,19 @@
+CREATE TABLE Categorie (
+	id INTEGER NOT NULL UNIQUE,
+	nom VARCHAR(40) NOT NULL,
+	PRIMARY KEY (id)
+);
+
+CREATE TABLE Produit(
+	id INTEGER NOT NULL UNIQUE,
+	nom VARCHAR(40) NOT NULL,
+	code  INTEGER NOT NULL UNIQUE,
+	categorie_id INTEGER NOT NULL UNIQUE,
+	PRIMARY KEY (id),
+	
+	CONSTRAINT produit_categorie_fk
+		FOREIGN KEY (categorie_id) 
+		REFERENCES Categorie(id)
+
+);
+
