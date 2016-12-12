a8dac066d3807d6faf60a80eaa4e62441605e15b
Coline Martin <colinemartin95@gmail.com>
2016-12-12 14:04:47 +0100

    modification id avec l'autoincrément

--- requeteCreationProduitsCategorie.sql
+++ requeteCreationProduitsCategorie.sql
@@ -5,7 +5,18 @@ INSERT INTO Categorie(id,nom) VALUES(3,'Velociraptor');
 
 INSERT INTO Produit VALUES(1,'gateau chocolat',12345,1);
 INSERT INTO Produit VALUES(2,'Paris Brest',23560,1);
-INSERT INTO Produit VALUES(3,'Moutarde',29632,2);
+INSERT INTO Produit VALUES(3,'Moutarde' 26932,2);
 INSERT INTO Produit VALUES(4,'Poivre',25236,2);
 INSERT INTO Produit VALUES(5,'Paul',27896,3);
 INSERT INTO Produit VALUES(6,'William',27896,3);
+
+/* Raccourcis pour remplir plusieurs champs
+INSERT INTO Produit VALUES
+	(1,'gateau chocolat',12345,1),
+	(2,'Paris Brest',23560,1),
+	(3,'Moutarde' 26932,2),
+	(4,'Poivre',25236,2),
+	(4,'Poivre',25236,2),
+	(6,'William',27896,3);
+	
+*/
